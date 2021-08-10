ef6b4abd34b7c87922dcaf460b0a48bca0bab016
kevin <kkovalchik.chusj@gmail.com>
2021-08-10 08:53:41 -0400

    add test for ordering intersects

--- UpSetPlotly/tests/test_set_functions.py
+++ UpSetPlotly/tests/test_set_functions.py
@@ -1,4 +1,4 @@
-from UpSetPlotly.set_functions import get_all_intersects
+from UpSetPlotly.set_functions import get_all_intersects, order_sample_intersects
 
 
 def test_get_all_intersects_returns():
@@ -39,3 +39,32 @@ def test_get_all_intersects_n():
         n += x['n']
     assert n == len(unique)
 
+
+def test_order_sample_intersects_increasing():
+    should_return = [{'samples': ('b',), 'elements': set(), 'n': 0},
+                     {'samples': ('a', 'c'), 'elements': set(), 'n': 0},
+                     {'samples': ('b', 'c'), 'elements': set(), 'n': 0},
+                     {'samples': ('a',), 'elements': {1}, 'n': 1},
+                     {'samples': ('a', 'b', 'c'), 'elements': {2}, 'n': 1},
+                     {'samples': ('c',), 'elements': {5, 6}, 'n': 2},
+                     {'samples': ('a', 'b'), 'elements': {3, 4}, 'n': 2}]
+    samples = [[1, 2, 3, 4], [2, 3, 4], [2, 5, 6]]
+    names = ['a', 'b', 'c']
+
+    intersects = get_all_intersects(samples, names)
+    assert order_sample_intersects(intersects, 'increasing') == should_return
+
+
+def test_order_sample_intersects_decreasing():
+    should_return = [{'samples': ('c',), 'elements': {5, 6}, 'n': 2},
+                     {'samples': ('a', 'b'), 'elements': {3, 4}, 'n': 2},
+                     {'samples': ('a',), 'elements': {1}, 'n': 1},
+                     {'samples': ('a', 'b', 'c'), 'elements': {2}, 'n': 1},
+                     {'samples': ('b',), 'elements': set(), 'n': 0},
+                     {'samples': ('a', 'c'), 'elements': set(), 'n': 0},
+                     {'samples': ('b', 'c'), 'elements': set(), 'n': 0}]
+    samples = [[1, 2, 3, 4], [2, 3, 4], [2, 5, 6]]
+    names = ['a', 'b', 'c']
+
+    intersects = get_all_intersects(samples, names)
+    assert order_sample_intersects(intersects, 'decreasing') == order_sample_intersects(intersects) == should_return
